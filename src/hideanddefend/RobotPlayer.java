7915d50bdd255682c17dc2cf192bccb389ad8580
BigBirdTheGreat <shreyshingala1@gmail.com>
2024-01-10 19:51:05 -0500

    NEW_HIDEY_SPAWNY_SUPER_AMAZINGNNGGNNGNG

--- src/hideanddefend/RobotPlayer.java
+++ src/hideanddefend/RobotPlayer.java
@@ -2,18 +2,8 @@ package hideanddefend;
 
 import battlecode.common.*;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
-/**
- * RobotPlayer is the class that describes your main robot strategy.
- * The run() method inside this class is like your main function: this is what we'll call once your robot
- * is created!
- */
 public strictfp class RobotPlayer {
     static int turnCount = 0;
     static final Random rng = new Random(6147);
@@ -28,6 +18,10 @@ public strictfp class RobotPlayer {
             Direction.WEST,
             Direction.NORTHWEST,
     };
+
+    static final int NUM_LOOKERS = 5;
+    static final Map<Integer, MapLocation> originalPositions = new HashMap<>();
+    static boolean returnToOriginalPositions = false;
 
     public static void run(RobotController rc) throws GameActionException {
         boolean isHider = ((rc.getID() % 2) == 0);
@@ -46,44 +40,21 @@ public strictfp class RobotPlayer {
                     if (rc.canSpawn(randomLoc)) rc.spawn(randomLoc);
                 } else {
                     if (!isHider) {
-                        MapLocation[] crumbLocations = rc.senseNearbyCrumbs(-1);
-                        if (crumbLocations.length != 0){
-                            rc.setIndicatorString("There are nearby crumbs! Yum!");
-                            Direction dir = rc.getLocation().directionTo(crumbLocations[0]);
-                            if (rc.canMove(dir)) rc.move(dir);
+                        if (returnToOriginalPositions) {
+                            returnToOriginalPositions(rc);
+                            returnToOriginalPositions = false;
                         } else {
-                            Direction dir = directions[rng.nextInt(directions.length)];
-                            MapLocation nextLoc = rc.getLocation().add(dir);
-                            if (rc.canMove(dir)){
-                                rc.move(dir);
-                            } else if (rc.canAttack(nextLoc)){
-                                rc.attack(nextLoc);
-                                System.out.println("Take that! Damaged an enemy that was in our way!");
-                            }
+                            moveAndAttack(rc);
                         }
                     } else {
-                        if (rc.getRoundNum() >= 10) {
-                            for (Direction d : directions) {
-                                MapLocation prevLoc = rc.getLocation().subtract(d);
-                                if (rc.canBuild(TrapType.EXPLOSIVE, prevLoc)) {
-                                    rc.build(TrapType.EXPLOSIVE, prevLoc);
-                                }
-                            }
-
-                            for (Direction d : directions) {
-                                MapLocation atLoc = rc.getLocation().subtract(d);
-                                if (rc.canAttack(atLoc)) {
-                                    rc.attack(atLoc);
-                                    System.out.println("Take that! Damaged an enemy that was in our way!");
-                                }
-                            }
-
-                            for (Direction d : directions) {
-                                MapLocation healLoc = rc.getLocation().subtract(d);
-                                if (rc.canHeal(healLoc)) {
-                                    rc.heal(healLoc);
-                                    System.out.println("Healed a dude");
-                                }
+                        if (rc.getRoundNum() < 150) {
+                            exploreMap(rc);
+                        } else {
+                            if (returnToOriginalPositions) {
+                                returnToOriginalPositions(rc);
+                                returnToOriginalPositions = false;
+                            } else {
+                                deployLookers(rc);
                             }
                         }
                     }
@@ -99,6 +70,75 @@ public strictfp class RobotPlayer {
 
             } finally {
                 Clock.yield();
+            }
+        }
+    }
+
+    public static void moveAndAttack(RobotController rc) throws GameActionException {
+        MapLocation[] crumbLocations = rc.senseNearbyCrumbs(-1);
+        if (crumbLocations.length != 0) {
+            rc.setIndicatorString("There are nearby crumbs! Yum!");
+            Direction dir = rc.getLocation().directionTo(crumbLocations[0]);
+            if (rc.canMove(dir)) rc.move(dir);
+        } else {
+            Direction dir = directions[rng.nextInt(directions.length)];
+            MapLocation nextLoc = rc.getLocation().add(dir);
+            if (rc.canMove(dir)) {
+                rc.move(dir);
+            } else if (rc.canAttack(nextLoc)) {
+                rc.attack(nextLoc);
+                System.out.println("Take that! Damaged an enemy that was in our way!");
+            }
+        }
+    }
+
+    public static void exploreMap(RobotController rc) throws GameActionException {
+        Direction randomDir = directions[rng.nextInt(directions.length)];
+        if (rc.canMove(randomDir)) {
+            rc.move(randomDir);
+        }
+    }
+
+    public static void deployLookers(RobotController rc) throws GameActionException {
+        if (originalPositions.isEmpty()) {
+            for (int i = 0; i < NUM_LOOKERS; i++) {
+                originalPositions.put(i, rc.getLocation());
+            }
+        }
+
+        int lookerIndex = rc.getID() % NUM_LOOKERS;
+        MapLocation originalPos = originalPositions.get(lookerIndex);
+        Direction dir = rc.getLocation().directionTo(originalPos);
+
+        if (rc.getRoundNum() > 150) {
+            if (rc.getLocation().distanceSquaredTo(originalPos) > 0) {
+                if (rc.canMove(dir)) {
+                    rc.move(dir);
+                } else {
+                    returnToOriginalPositions = true;
+                }
+            } else {
+                returnToOriginalPositions = true;
+            }
+        } else {
+            if (rc.getLocation().distanceSquaredTo(originalPos) > 0) {
+                if (rc.canMove(dir)) {
+                    rc.move(dir);
+                }
+            }
+        }
+    }
+
+    public static void returnToOriginalPositions(RobotController rc) throws GameActionException {
+        for (int i = 0; i < NUM_LOOKERS; i++) {
+            int lookerID = (rc.getID() + i) % NUM_LOOKERS;
+            MapLocation originalPos = originalPositions.get(i);
+            Direction dir = rc.getLocation().directionTo(originalPos);
+
+            if (rc.getLocation().distanceSquaredTo(originalPos) > 0) {
+                if (rc.canMove(dir)) {
+                    rc.move(dir);
+                }
             }
         }
     }
